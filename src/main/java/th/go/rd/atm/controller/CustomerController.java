e3aeb112a59a51f96b5694a0af982b330aeceb25
e3xtreme <e3xtreme@gmail.com>
2020-08-26 14:15:15 +0700

    Add Model Controller Commit

--- src/main/java/th/go/rd/atm/controller/CustomerController.java
+++ src/main/java/th/go/rd/atm/controller/CustomerController.java
@@ -2,6 +2,9 @@ package th.go.rd.atm.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -9,13 +12,18 @@ import java.util.ArrayList;
 @Controller
 public class CustomerController {
 
-    @RequestMapping("/customer")
+    ArrayList<Customer> customers = new ArrayList<>();
+
+    @GetMapping("/customer")
     public String getCustomer(Model model){
-        ArrayList<String> customer = new ArrayList<>();
-        customer.add("Peter Corp");
-        customer.add("Nancy Girl");
-        customer.add("Riddick Gay");
-        model.addAttribute("allCustomers", customer);
+        model.addAttribute("allCustomers", customers);  //ส่งโมเดลที่เก็บออปเจ็ก customers ที่ชื่อ "allCustomers" ไปให้ view
         return "customer";  // customer.html
+    }
+
+    @PostMapping("/customer")
+    public String registerCustomer(@ModelAttribute Customer customer, Model model){
+        customers.add(customer);
+        model.addAttribute("allCustomers", customers);
+        return "redirect:customer";
     }
 }
